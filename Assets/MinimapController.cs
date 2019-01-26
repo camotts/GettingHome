4a042e1d5541698bfe9a250d90d4af8801288867
camotts <otts.cameron@gmail.com>
2019-01-26 04:30:19 -0600

    added basic tiger ai and minimap

---
+++ Assets/MinimapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    [SerializeField] private float Y = 150;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, Y, transform.localPosition.z);
+    }
+}
--- Assets/Well.cs
+++ Assets/Well.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Well : MonoBehaviour, IDrinkable
+public class Well : MonoBehaviour, IDrinkable, IInteractable
 {
     [SerializeField] private float Hydration = 100;
 
     public float Drink()
     {
         return Hydration;
+    }
+
+    public string GetInteractionText()
+    {
+        return "Press F to drink water";
     }
 }
